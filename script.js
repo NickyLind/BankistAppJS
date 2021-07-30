2753d5b11bc76a9850761af15f8fac16ed53cdef
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-30 12:56:16 -0700

    add internationalization for all displayed currencies

--- script.js
+++ script.js
@@ -97,6 +97,13 @@ const formatMovementDate = function(date, locale) {
   }
 }
 
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  }).format(value);
+}
+
 
 
 // Function for displaying rows of deposits/withdrawls
@@ -109,13 +116,14 @@ const displayMovements = function(acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
     const date = new Date(acc.movementsDates[i])
     const displayDate = formatMovementDate(date, acc.locale)
+    const formattedMov = formatCur(mov, acc.locale, acc.currency)
 
 
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
       <div class="movements__date">${displayDate}</div>
-      <div class="movements__value">${mov.toFixed(2)}€</div>
+      <div class="movements__value">${formattedMov}</div>
     </div>
     `
     containerMovements.insertAdjacentHTML('afterbegin', html)
@@ -128,12 +136,12 @@ const calcDisplaySummary = function(acc){
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0)
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
   .filter(mov => mov < 0)
   .reduce((acc, mov) => acc - mov, 0)
-  labelSumOut.textContent = `${out.toFixed(2)}€`
+  labelSumOut.textContent = formatCur(out, acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -141,7 +149,7 @@ const calcDisplaySummary = function(acc){
     .filter((int, i, array) => int >= 1
     )
     .reduce((acc, int) => acc + int, 0)
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 }
 
 // Function for displaying total account balance
@@ -149,7 +157,7 @@ const calcDisplayBalance = function(acc) {
   acc.balance = acc.movements.reduce(function(acc, mov) {
     return acc + mov
   }, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency)
 };
 
 // Function for updating the UI
